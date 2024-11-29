7c02797336d2ca1d756e4969dcea60e4ee870ac9
Piyush Kumar Jaiswal <piyushkumarj48@gmail.com>
2024-11-29 20:54:12 +0530

    my new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Myfile from './component/Myfile'
 
 const App = () => {
   return (
     <>
-    
+    <Myfile/>
     <div>Radhe Radhe</div>
     <div>Radhe Radhe</div>
     <div>Radhe Radhe</div>
---
+++ src/component/Myfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Myfile = () => {
+  return (
+    <div>this is myfile</div>
+  )
+}
+
+export default Myfile
