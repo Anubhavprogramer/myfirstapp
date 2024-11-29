03cb2d98ea4c23245d0d0ea293869a8a1d22afb3
Piyush Kumar Jaiswal <piyushkumarj48@gmail.com>
2024-11-29 20:58:17 +0530

    my new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Myfile from './component/Myfile'
+import Temp from './component/Temp'
 
 const App = () => {
   return (
     <>
     <Myfile/>
+    <Temp/>
     <div>Radhe Radhe</div>
     <div>Radhe Radhe</div>
     <div>Radhe Radhe</div>
